8fb4483d84f2573744a289f803b4dd7a382236c9
Aman <akoua@umich.edu>
2023-08-26 11:06:20 -0400

    Succsessfully add and delete tracks from backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { MongoClient, GridFSBucket, ObjectId } = require('mongodb');
 const userRouter = require('./routes/user');
+const fs = require('fs');
 const cors = require('cors');
 
 require('dotenv').config();
@@ -16,7 +18,66 @@ app.get('/', (req, res) => {
     res.status(200).json({ message: "Hello to prodlodge!" })
 })
 
-mongoose.connect(process.env.MONGO_URI).then(() => {
+let getGridFSBucket = async () => {
+
+    const client = new MongoClient(process.env.MONGO_URI);
+
+    try {
+
+        // works for uploading a file!
+
+        await client.connect();
+        const db = client.db("ProdCluster")
+        const bucket = new GridFSBucket(db);
+
+        const filePath = "C:/Personal Use/Programming/ProdLodge/Testing/AudioInterface/Frontend/src/assets/songs/stems/master.mp3";
+        const uploadStream = bucket.openUploadStream("Far cry 4 remix");
+        const fileStream = fs.createReadStream(filePath);
+        fileStream.pipe(uploadStream);
+
+        uploadStream.on("finish", () => {
+            console.log("Upload successful!");
+            client.close();
+        })
+
+        //--------------------------------------------------------------------
+
+
+        // seems to work for DLing a song!
+        // await client.connect();
+        // const db = client.db("ProdCluster")
+        // const bucket = new GridFSBucket(db);
+
+        // const fileId = "Far cry 4 remix";
+        // const dlPath = "C:/Personal Use/Programming/ProdLodge/Testing/AudioInterface/Frontend/src/assets/songs/stems/DLTrack.mp3"
+        // const dlStream = bucket.openDownloadStreamByName(fileId);
+        // const fileStream = fs.createWriteStream(dlPath);
+        // dlStream.pipe(fileStream);
+
+        // fileStream.on("finish", () => {
+        //     console.log("download successful!");
+        //     client.close();
+        // })
+
+        //-------------------------------------------------------------------------------
+
+        // seems to work for deleting a song!
+        // await client.connect();
+        // const db = client.db("ProdCluster")
+        // const bucket = new GridFSBucket(db);
+
+        // await bucket.delete(new ObjectId('64e9799c8ebf1a1d3fa1d40a'));
+
+    } catch (e) {
+        console.log(e);
+    }
+
+}
+
+mongoose.connect(process.env.MONGO_URI).then(async () => {
+
+    await getGridFSBucket();
+
     app.listen(process.env.PORT, () => {
         console.log("Listening on port " + process.env.PORT);
     })
---
+++ Backend/models/userProfileModel.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcrypt");
+
+const Schema = mongoose.Schema;
+
+const userProfileSchema = new Schema({
+    // TODO : Return after profile pictures have been uploaded
+})
