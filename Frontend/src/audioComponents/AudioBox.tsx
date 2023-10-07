5dd62bcb69bdb86c11c0f60516efedd9e940464c
Aman <akoua@umich.edu>
2023-10-07 12:14:12 -0400

    Add gradient to audio box and fixed hope page song selector menu length

--- Frontend/src/audioComponents/AudioBox.tsx
+++ Frontend/src/audioComponents/AudioBox.tsx
@@ -912,8 +912,8 @@ const AudioBox = ({ songData, isPageSwitched, setIsSongPayloadSet }: Props) => {
   };
 
   const SongDataContainerClassName = !isSongDataContainerHover
-    ? "w-full h-9 overflow-hidden bg-prodSecondary"
-    : "w-full h-24 overflow-hidden bg-prodSecondary"; // toggle height with tailwind
+    ? "w-full h-9 overflow-hidden bg-gradient-to-b from-prodSecondary to-prodPrimary"
+    : "w-full h-24 overflow-hidden bg-gradient-to-b from-prodSecondary to-prodPrimary"; // toggle height with tailwind
 
   // SongDataContainerStyle.height = isExpanded ? "100px" : "40px";
 
