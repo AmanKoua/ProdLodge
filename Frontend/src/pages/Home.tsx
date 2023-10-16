bb039d2d28ffc8bdb596fe1e24dcbff6e6f55d8e
Aman <akoua@umich.edu>
2023-10-15 20:19:42 -0400

    Changed colors for song data container

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -119,10 +119,7 @@ const Home = () => {
   };
 
   return (
-    <div
-      className="shadow-blue-200 bg-gradient-to-b from-prodPrimary to-prodSecondary shadow-xl w-full sm:w-8/12 h-screen mr-auto ml-auto pb-4 hide-scrollbar overflow-y-scroll"
-      // style={{ backgroundColor: "#F8FBFF" }}
-    >
+    <div className="shadow-blue-200 bg-gradient-to-b from-prodPrimary to-prodSecondary shadow-xl w-full sm:w-8/12 h-screen mr-auto ml-auto pb-4 hide-scrollbar overflow-y-scroll">
       {/* Do not allow the displaying of audioBoxes on mobile sized screens */}
 
       <div className="w-10/12 h-7 ml-auto mr-auto mt-2 overflow-hidden flex justify-around">
