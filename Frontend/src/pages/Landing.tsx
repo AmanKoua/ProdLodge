330eefc8c2faa22bd85ebedd49105c1028aa23a3
Aman <akoua@umich.edu>
2023-11-24 00:03:59 -0500

    initial locomotive scroll setup and test

--- Frontend/src/pages/Landing.tsx
+++ Frontend/src/pages/Landing.tsx
@@ -1,9 +1,13 @@
-import React, { useRef } from "react";
-import { useNavigate } from "react-router-dom";
-import { LocomotiveScrollProvider } from "react-locomotive-scroll";
+import React, { useRef, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import {
+  LocomotiveScrollProvider,
+  useLocomotiveScroll,
+} from "react-locomotive-scroll";
 import "locomotive-scroll/dist/locomotive-scroll.css";
 
 const Landing = () => {
+  const location = useLocation();
   const containerRef = useRef(null);
   const navigate = useNavigate();
 
@@ -14,30 +18,41 @@ const Landing = () => {
         // ... all available Locomotive Scroll instance options
       }}
       watch={[
-        "router.asPath",
+        location.pathname,
         //..all the dependencies you want to watch to update the scroll.
         //  Basicaly, you would want to watch page/location changes
         //  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
       ]}
       containerRef={containerRef}
     >
-      <div
-        data-scroll-container="true"
-        ref={containerRef}
-        className="bg-gradient-to-b from-prodPrimary to-prodSecondary shadow-xl shadow-blue-200 w-full mr-auto ml-auto pb-4 hide-scrollbar overflow-y-hidden"
-        style={{ minHeight: "115vh" }}
-      >
-        <h1 className="w-max border-2 ml-auto mr-auto">
-          Welcome to ProdLodge!
-        </h1>
-        <button
-          onClick={() => {
-            navigate("/home");
-          }}
+      <main data-scroll-container ref={containerRef}>
+        <div
+          data-scroll-section
+          className="bg-gradient-to-b from-prodPrimary to-prodSecondary shadow-xl shadow-blue-200 w-full mr-auto ml-auto pb-4 hide-scrollbar overflow-y-hidden"
+          style={{ minHeight: "115vh" }}
         >
-          lmao
-        </button>
-      </div>
+          <div className="h-max w-max ml-auto mr-auto mt-10">
+            <h1 className="w-max ml-auto mr-auto mt-auto text-6xl">
+              Welcome to ProdLodge!
+            </h1>
+            <h1
+              data-scroll
+              data-scroll-speed="-5"
+              className="w-max ml-auto text-xl"
+            >
+              Welcome to ProdLodge!
+            </h1>
+          </div>
+
+          <button
+            onClick={() => {
+              navigate("/home");
+            }}
+          >
+            lmao
+          </button>
+        </div>
+      </main>
     </LocomotiveScrollProvider>
   );
 };
