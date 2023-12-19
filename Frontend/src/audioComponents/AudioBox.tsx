f9e1fe051e1a079d5132b4b9f72c9db50e498dae
Aman <akoua@umich.edu>
2023-12-18 21:58:20 -0500

    round audio box song data container

--- Frontend/src/audioComponents/AudioBox.tsx
+++ Frontend/src/audioComponents/AudioBox.tsx
@@ -959,8 +959,8 @@ const AudioBox = ({ songData, isPageSwitched, setIsSongPayloadSet }: Props) => {
   };
 
   const SongDataContainerClassName = !isSongDataContainerHover
-    ? "w-full h-9 overflow-hidden bg-gradient-to-b from-prodSecondary to-blue-50"
-    : "w-full h-24 overflow-hidden bg-gradient-to-b from-prodSecondary to-blue-50"; // toggle height with tailwind
+    ? "w-full h-9 rounded-t-xl overflow-hidden bg-gradient-to-b from-prodSecondary to-blue-50"
+    : "w-full h-24 rounded-t-xl overflow-hidden bg-gradient-to-b from-prodSecondary to-blue-50"; // toggle height with tailwind
 
   // SongDataContainerStyle.height = isExpanded ? "100px" : "40px";
 
